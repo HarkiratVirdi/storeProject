25ea500fd700ac1aaed95d2aa065a88d587be6a7
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2022-02-02 00:08:10 -0500

    middleware response change

--- src/middlewares/checkIfUserLogged.ts
+++ src/middlewares/checkIfUserLogged.ts
@@ -1,7 +1,7 @@
 import { errorResponse } from "../utils/readDb";
 
  const isUserLogged = (req, res, next) => {
-       return req.headers.authorization ? next() : next();
+       return req.headers.authorization ? next() : res.json(errorResponse(401, "User token missing"));
 }
 
 export default isUserLogged;
